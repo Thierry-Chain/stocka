2ea4186929bf5c51ea428c4a4222511401b8c49e
Thierry-Chain <irambonat0@gmail.com>
2021-07-22 17:50:16 +0200

    finished nav bar before login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
+import {Route,Switch} from 'react-router-dom'
+import {Container,Flex} from '@chakra-ui/react'
+import NavBar from 'components/NavBar'
 
 function App() {
   return (
-    <div className="App">
-     stock management app
+     <Flex direction={['column','column','row']} h="100vh"  maxW="container.xl" overflow="hidden" className="App">
+
+     <Flex bg="#020202" w={['100vw','100vw','25%']}>
+       <NavBar  />
+     </Flex>
+
+     <Flex>
+       section 2
+     </Flex>
     
-    </div>
+     </Flex>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import {BrowserRouter} from 'react-router-dom'
 import {ChakraProvider} from '@chakra-ui/react'
 import {QueryClientProvider,QueryClient} from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import {Provider} from 'react-redux'
 import  store  from 'Redux/store'
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -20,7 +22,9 @@ ReactDOM.render(
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
         <Provider store={store}>
-          <App />
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
         </Provider>
         <ReactQueryDevtools initialIsOpen={false} />
       </ChakraProvider>
