6ef4d889285d7c4bf92d345c01547fc867a12c47
Thierry-Chain <irambonat0@gmail.com>
2022-10-23 21:54:09 +0200

    improved payment page after login

--- src/components/loggedIn/Payment.jsx
+++ src/components/loggedIn/Payment.jsx
@@ -1,19 +1,28 @@
-import { Container, Text } from '@chakra-ui/react'
-
+import { Container, Text } from '@chakra-ui/react';
 
 export default function Payment() {
-  return <Container h="88vh" minW="full" pl="0" pr="0">
-    <Text
-      fontWeight="bold"
-      fontSize="larger"
-      bgGradient="linear(to-l,blue.300,blue.800)"
-      bgClip="text"
-      textAlign="center"
-      p="2"
-    >
-      Payment
+  return (
+    <Container h="88vh" minW="full" pl="0" pr="0">
+      <Text
+        fontWeight="bold"
+        fontSize="larger"
+        bgGradient="linear(to-l,blue.300,blue.800)"
+        bgClip="text"
+        textAlign="center"
+        p="2"
+      >
+        Payment
       </Text>
-    <Text fontSize="larger"
-      bgGradient="linear(to-l,blue.200,blue.700)" textAlign="center">Sorry this service is under construction</Text>
-  </Container>
+      <Text
+        fontSize="larger"
+        bgGradient="linear(to-l,gray.200,gray.400)"
+        textAlign="center"
+        py="16"
+        fontWeight="bold"
+        color="blackAlpha.700"
+      >
+        Sorry this service is under construction
+      </Text>
+    </Container>
+  );
 }
